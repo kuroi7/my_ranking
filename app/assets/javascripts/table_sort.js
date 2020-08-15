ff7a82530abea55c5c427a9c1044b5038a5ea7af
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-08-15 13:55:32 +0900

    jsファイルのコメント削除

--- app/assets/javascripts/table_sort.js
+++ app/assets/javascripts/table_sort.js
@@ -1,6 +1,4 @@
-// $(function(){
 window.addEventListener('DOMContentLoaded', function(){
-  // console.log('こんにちは');
   $('.table-sortable').sortable({
     update: function (e, ui) {
       var item = ui.item;
@@ -17,23 +15,3 @@ window.addEventListener('DOMContentLoaded', function(){
     }
   });
 });
-
-// $(function () {
-//   $('.table-sortable').sortable({
-//     update: function (e, ui) {
-//       let item = ui.item;
-//       let item_data = item.data();
-//       let params = { _method: 'put' };
-//       params[item_data.modelName] = { row_order_position: item.index() }
-//       $.ajax({
-//         type: 'POST',
-//         url: item_data.updateUrl,
-//         dataType: 'json',
-//         data: params
-//       });
-//     },
-//     stop: function (e, ui) {
-//       ui.item.children('td').not('.item__status').effect('highlight', { color: "#FFFFCC" }, 500)
-//     }
-//   });
-// });
